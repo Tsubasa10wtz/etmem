d20020488c2cd7acffad47a727a566c01aeae28a
liubo <liubo254@huawei.com>
2022-03-05 13:40:32 +0800

    etmem: add code of testcase
    add code of testcase of etmem cslide functions.
    modify testcode in etmem common llt.
    
    Signed-off-by: liubo <liubo254@huawei.com>

--- etmem/test/etmem_slide_ops_llt_test/test_slide_ops.sh
+++ etmem/test/etmem_slide_ops_llt_test/test_slide_ops.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-#
-#Level: 0
-#Type: auto
-#Description: start 多个不同的project
+
+# /******************************************************************************
+#  * Copyright (c) Huawei Technologies Co., Ltd. 2019-2021. All rights reserved.
+#  * etmem is licensed under the Mulan PSL v2.
+#  * You can use this software according to the terms and conditions of the Mulan PSL v2.
+#  * You may obtain a copy of Mulan PSL v2 at:
+#  *     http://license.coscl.org.cn/MulanPSL2
+#  * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+#  * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+#  * PURPOSE.
+#  * See the Mulan PSL v2 for more details.
+#  * Author: liubo
+#  * Create: 2021-11-29
+#  * Description: test shell for etmem_slide_ops_llt
+#  ******************************************************************************/
 
 set +e
 
